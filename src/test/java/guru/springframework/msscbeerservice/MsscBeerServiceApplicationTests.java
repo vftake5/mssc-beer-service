811e0598cf162c60d2fabf310ced1342515db8e9
ViFTake5 <viftake5@gmail.com>
2021-12-20 17:06:34 +0100

    Initial project add. Closes #1

---
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
